351f38210841b7d890694ea588a13e167b6a509e
Sulaiman-Gaskell <103843022+Sulaiman-Gaskell@users.noreply.github.com>
2023-10-29 20:55:28 +0000

    Update Program.cs

--- Quizmaker/Program.cs
+++ Quizmaker/Program.cs
@@ -1,2 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
+class Program
+{
+    static void Main()
+    {
+        string currentDirectory = Directory.GetCurrentDirectory();
+        Console.WriteLine(currentDirectory);
+        string filePath = Path.Combine(currentDirectory, "questions.txt");
+        List<string> lines = File.ReadAllLines(filePath).ToList();
+
+        Random rand = new Random();
+        int counter = 0;
+        int correct = 0;
+
+        while (counter < 5)
+        {
+            int randomIndex = rand.Next(0, lines.Count);
+            string randomLine = lines[randomIndex];
+            string[] qAndA = randomLine.Split(',');
+            Console.WriteLine(qAndA[0]);
+            string answer = Console.ReadLine();
+            if (answer.ToLower() == qAndA[1].ToLower()){correct++;}
+            counter++;
+        }
+        Dictionary<int, string> grades = new Dictionary<int, string>();
+        grades[5] = "A";
+        grades[4] = "B";
+        grades[3] = "C";
+        grades[2] = "D";
+        grades[1] = "E";
+        grades[0] = "F";
+
+       foreach (int key in grades.Keys) 
+       {
+            if (key == correct)
+            {
+                string grade = grades[key];
+                Console.WriteLine(grade);
+            }
+       }
+            
+    }
+}
